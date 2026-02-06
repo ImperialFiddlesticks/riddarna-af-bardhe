6f5c1cdbc72e588d68f69c6a529c889c99e23869
ImperialFiddlesticks <bleckert.andrea@gmail.com>
2026-02-06 13:38:00 +0100

    Removed unused imports

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
-import { Outlet, createRootRoute, Link } from "@tanstack/react-router";
-import { NavButton } from "@/components/NavButton";
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+
 import { Header } from "@/components/Header";
 export const Route = createRootRoute({
   component: RootComponent,
